cf72ee9a0152d2cb19fa898f17c92451a3a04562
83-Caleb <por24419@uvg.edu.gt>
2026-02-17 23:42:51 -0600

    Lab 03 Terminado

--- Lab_03/Lab_03/main.asm
+++ Lab_03/Lab_03/main.asm
@@ -7,6 +7,8 @@
 .dseg // Se guardará el estado previo de los botones en la SRAM
 botones: .byte 1
 contador: .byte 1
+L_ciclos: .byte 1
+unidades_7seg: .byte 1
 
 // *********************************************************************************************************************************************//
 .cseg
@@ -15,6 +17,9 @@ contador: .byte 1
 
 .org PCI1addr // Interrupción para pin change en PORTC
 	RJMP ISR_PCINT1
+
+.org OVF0addr // Interrupción por timer
+	RJMP ISR_timer
 	
 	start:
 	CLI
@@ -24,6 +29,20 @@ contador: .byte 1
 		OUT SPL, R16 // Stack pointer low
 		LDI R16, HIGH(RAMEND) // LDI R16 0x08
 		OUT SPH, R16 // Ahora este tiene 0x08FF
+
+	// Configuración del oscilador a 2MHz
+		LDI R16, (1<<CLKPCE) // Carga 0x80 (1000_0000)
+		STS CLKPR, R16 // Enable del prescaler
+		LDI R16, 0x03 // Set del CLKPS2 (0000_0011), que divide entre 8
+		STS CLKPR, R16
+
+	// Timer
+		LDI R16, (1<<CS02) // CLK a 256
+		OUT TCCR0B, R16
+		LDI R16, 100 // Empezamos a contar en 100
+		OUT TCNT0, R16
+		LDI R16, (1<<TOIE0) // Enable de la interrupción Timer/Counter0
+		STS TIMSK0, R16
 		
 	// DESHABILITAR ADC (Si no, no se puede usar PORTC como pines digitales (Se les conectaron las LEDs del contador y los botones)
 		lds r16, ADCSRA
@@ -77,6 +96,13 @@ contador: .byte 1
 		LPM R16, Z // Guardar el número actual (0 en el display)
 		OUT PORTD, R16 // Mostrar el número 0
 
+		LDI R16, 0x00
+		STS contador, R16
+		LDI R16, 0x00          ; <-- Esto falta
+		STS unidades_7seg, R16 ; <-- Agregar estas dos líneas
+		LDI R16, 0x00
+		STS L_ciclos, R16      ; También considera inicializar L_ciclos por la misma razón
+
 		SEI // Habilitar interrupciones
 
 // *********************************************************************************************************************************************//
@@ -88,6 +114,7 @@ contador: .byte 1
 // *********************************************************************************************************************************************//
 // Interrupciones
 
+// Pin-Change
 	ISR_PCINT1:
 		PUSH R16
 		IN   R16, SREG
@@ -152,6 +179,54 @@ contador: .byte 1
 		OUT PORTC, R16		
 		RJMP ISR_return
 
+// Por timer
+	ISR_timer:
+		PUSH R16
+		IN   R16, SREG
+		PUSH R16
+		PUSH R17
+		PUSH R18
+
+		LDS R17, unidades_7seg
+		LDS R16, L_ciclos
+		INC R16
+		CPI R16, 50 // Son iguales si ya pasaron 50 ciclos de 20ms
+		BRNE Regreso_ISR_timer
+		LDI R16, 0x00 // Reiniciar el contador de ciclos
+
+		INC R17
+		CPI R17, 0x10 // Si el display ya pasó de F, regresamos a 0
+		BREQ Overflow
+		RJMP Display_unidades
+
+	Regreso_ISR_timer:
+
+		LDI R18, 100 // Empezamos a contar en 100
+		OUT TCNT0, R18
+
+		LDI ZH, HIGH(Table_7seg<<1)	// Regresamos el valor de Z a 0 en el display
+		LDI ZL, LOW(Table_7seg<<1)
+
+		STS L_ciclos, R16 // Guardamos los nuevos valores del contador de ciclos y el display
+		STS unidades_7seg, R17
+		POP R18
+		POP R17
+		POP R16
+		OUT  SREG, R16
+		POP  R16
+		RETI
+
+	Display_unidades:
+
+		ADD ZL, R17
+		LPM R18, Z // Guardar el número actual
+		OUT PORTD, R18 // Mostrar el número 0
+		RJMP Regreso_ISR_timer
+
+	Overflow:
+		LDI R17, 0x00
+		RJMP Display_unidades
+		
 // *********************************************************************************************************************************************//
 // Tablas de datos
 
